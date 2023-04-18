9dbc1ee7de5f23ddf68d7a71f6bdd069fb128334
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-04-18 15:08:34 -0300

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int y = 90;
+        int z = 80;
+        int i = 70;
+        int u = 60;
+
+        Console.WriteLine("What is your score? ");
+        string score = Console.ReadLine();
+        int number = int.Parse(score);
+
+        
+
+
+        if (number >= y)
+        {
+            Console.WriteLine("A");
+        }
+        else if (number >= z)
+        {
+            Console.WriteLine("B");
+        }
+        else if (number >= i)
+        {
+            Console.WriteLine("C");
+        }
+        else if (number >= u)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
