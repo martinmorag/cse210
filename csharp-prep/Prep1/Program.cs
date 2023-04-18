3b7737e009fb83a9f2f46c022a42d831f9cd005f
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-04-18 15:07:55 -0300

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+    
+        {
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        Console.WriteLine($"My name is {last_name}, {first_name} {last_name}.");
+        }
