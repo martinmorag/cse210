0aadb76e2b381cc840df0778965e649b83de1210
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-04-19 18:06:36 -0300

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 5;
+        
+        Console.WriteLine("What is the magic number? ");
+        string magic = Console.ReadLine();
+        int number = int.Parse(magic);
+
+        if (number > guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (number < guess)
+        {
+            Console.WriteLine("Higher");
+        }
     }
 }
