9ccefc2dc6e633957755e22274fdd1a0a91df347
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-04-20 15:18:32 -0300

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,51 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+    DisplayWelcomeMessage();
+
+    string name = DisplayUsername();
+
+    int integer = DisplayUsernumber();
+  
+    int square = Square(integer);
+    
+
+    Result(name, square);
+
+
+
+    static  void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program");
     }
+    static string DisplayUsername()
+    {
+        Console.WriteLine("What is your name? ");
+        string username = Console.ReadLine();
+
+        return username;
+    }
+    static int DisplayUsernumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int Square(int integer)
+    {
+        int Square = integer * integer;
+        return Square;
+    }
+    static void Result(string name, int number)
+    {
+        Console.WriteLine($"The square number of your favorite number Mr {name} is {number}");
+    }
+
+
+    }
+     
 }
