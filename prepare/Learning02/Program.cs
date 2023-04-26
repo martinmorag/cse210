ed9fc408fb99ac267c394c0205b5ce77dd9c77fc
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-04-25 22:45:31 -0300

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Customer Support Agent";
+        job1._company = "Claro";
+        job1._startYear = 2012;
+        job1._endYear = 2014;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sales Agent";
+        job2._company = "Accenture";
+        job2._startYear = 2014;
+        job2._endYear = 2016;
+
+        Resume resume = new Resume();
+        resume._name = "Martin Moraga";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        
+        resume.Display();
+
+    }  
+    
 }
