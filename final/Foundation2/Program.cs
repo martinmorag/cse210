6f09e2d06d534b820cf234811c2119d37e6989d8
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-07-03 18:53:02 -0300

    Update Program.cs

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        
+        Order Order = new Order();
+        Product order = new Product();
+        Address customer = new Address();
+        Customer person = new Customer();
+
+        // FIRST ORDER
+        
+        Order.TheOrder("Selene Lezcano", "Apple", "Watermelon", "Pear");
+        Order.PackagingLabel("Apple", "Watermelon", "Pear");
+        Order.ShippingLabel("Russell Crowe St.", "Cumberland", "California", "USA");
+        Order.ShippingCost();  
+
+        // SECOND ORDER
+
+        Order.TheOrder("Martin Moraga", "Banana", "Watermelon", "Watermelon");
+        Order.PackagingLabel("Banana", "Watermelon", "Watermelon");
+        Order.ShippingLabel("Louis Vuitton", "Virasoro", "Corrientes", "Argentina");
+        Order.ShippingCost();    
     }
 }
