d009bd27862384caff26bcd4aa7cfa86badd4783
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-07-07 01:34:20 -0300

    Update Program.cs

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Lectures lecture = new Lectures("The Big Newsflash", "September 10th", "18:00 h", "Lecture", "47th St.", "Ronaldo Gonzalez", "200");
+        lecture.StandardDetails();
+
+        Receptions reception = new Receptions("The Bombastic party", "August 25th", "20:00 h", "Reception", "Jupiter Avenue", "thebestparty@gmail.com");
+        reception.ReceptionDetails();
+
+        Outdoors outdoor = new Outdoors("Nature Friends", "July 12th", "15:00 h", "Outdoor Gathering", "Yanika Park", "Sunny");
+        outdoor.ShortDescription();
     }
 }
