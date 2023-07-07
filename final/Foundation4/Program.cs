3c112d6aa0a1d089e17590faf69a79029ec831fb
martinmorag <113954312+martinmorag@users.noreply.github.com>
2023-07-07 01:34:43 -0300

    Update Program.cs

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        
+        Running race = new Running("Running", 4.8, 30);
+        Swimming dive = new Swimming("Siwmming", 20, 40);
+        Biycles ride = new Biycles("Cycling", 6, 25);
+        race.TheList();
+        dive.TheList();
+        ride.TheList();
     }
 }
